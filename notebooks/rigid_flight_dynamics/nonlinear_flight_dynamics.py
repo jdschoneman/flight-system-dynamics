9481fbee85a23c770f9ec86ccdd72e4ea33fc8c0
Joe <jdschoneman@gmail.com>
2025-12-31 20:40:57 -0600

    Added "Eigenvalues and Eigenvectors" notebook

--- notebooks/rigid_flight_dynamics/nonlinear_flight_dynamics.py
+++ notebooks/rigid_flight_dynamics/nonlinear_flight_dynamics.py
@@ -24,16 +24,16 @@ def eom_6dof(time, state, aircraft, output_mode = False, rho = RHO_SL_SLUG_FT3, 
         Current time, supplied for purposes of computing aircraft mass property and aerodynamic information
     state : array
         State vector with order [u, v, w, p, q, r, phi, theta, psi, xe, ye, ze] corresponding to four sets
-        of equations: Body-frame translational, body-frame rotational, rotational kinematic, 
+        of equations: Body-frame translational, body-frame rotational, rotational kinematic,
         and translational kinematic
     aircraft : object
         An aircraft class that returns its mass properties, thrust forces, and aerodynamic coefficients as
-        functions of the time and state vector via the methods "get_mass_props," "get_thrust," and 
+        functions of the time and state vector via the methods "get_mass_props," "get_thrust," and
         "get_aero_coeffs" respectively
     output_mode : bool, optional
         Flag to return selected outputs rather than the state vector time derivativel, default is False
     rho : float, optional
-        At-altitude density. The default is a sea level value of 0.00238 slug/ft^3; for SI units change 
+        At-altitude density. The default is a sea level value of 0.00238 slug/ft^3; for SI units change
         to 1.225 kg/m^3
     g : float, optional
         Gravitational acceleration. The default is a standard value of 32.17 ft/s^2; for SI units change
@@ -81,7 +81,7 @@ def eom_6dof(time, state, aircraft, output_mode = False, rho = RHO_SL_SLUG_FT3, 
     # Rotational equations; the solution for pdot and rdot coupled by Ixz is obtained
     # from a 2x2 matrix inversion
     qdot = (M - p*r*(Ixx - Izz) - (p*p - r*r)*Ixz)/Iyy
-    pdot, rdot = np.linalg.solve([[Ixx, -Ixz], [-Ixz, Izz]], 
+    pdot, rdot = np.linalg.solve([[Ixx, -Ixz], [-Ixz, Izz]],
                                  [L - q*r*(Izz - Iyy) + q*p*Ixz,
                                   N - p*q*(Iyy - Ixx) - q*r*Ixz])
 
@@ -98,31 +98,31 @@ def eom_6dof(time, state, aircraft, output_mode = False, rho = RHO_SL_SLUG_FT3, 
     zedot = -u*sin_theta + v*cos_theta*sin_phi + w*cos_theta*cos_phi
 
     if output_mode:
-        
+
         V = np.sqrt(u*u + v*v + w*w)
         beta = np.arcsin(v/V)
         alpha = np.arcsin(w/V)
-        
+
         ax = Fx/(mass*g)
         ay = Fy/(mass*g)
         az = Fz/(mass*g)
-        
+
         roll_control = aircraft.roll_control(time, state)
         pitch_control = aircraft.pitch_control(time, state)
         yaw_control = aircraft.yaw_control(time, state)
-        return np.array([V, alpha, beta, 
-                         p, q, r, 
+        return np.array([V, alpha, beta,
+                         p, q, r,
                          phi, theta, psi,
-                         xe, ye, ze, 
+                         xe, ye, ze,
                          ax, ay, az,
                          roll_control, pitch_control, yaw_control])
     else:
-        return np.array([udot, vdot, wdot, 
+        return np.array([udot, vdot, wdot,
                          pdot, qdot, rdot,
                          phidot, thetadot, psidot,
                          xedot, yedot, zedot])
-                                      
-    
+
+
 
 class AircraftLinear:
 
@@ -230,9 +230,9 @@ class AircraftLinear:
 
         self.Clr = kwargs.get('Clr', 0.)
         self.Cnr = kwargs.get('Cnr', 0.)
-                              
 
-    def set_aero_controls(self, 
+
+    def set_aero_controls(self,
                           roll = lambda time, state: 0.,
                           pitch = lambda time, state: 0.,
                           yaw = lambda time, state: 0.):
@@ -298,16 +298,16 @@ def get_initial_state(airspeed, alpha, gamma = 0.0):
     return np.array([airspeed*np.cos(alpha), 0., airspeed*np.sin(alpha), # u, v, w
                      0., 0., 0.,                                    # p, q, r
                      0., theta, 0.,                                 # phi, theta, psi
-                     0., 0., 0.])                                   # Xe, Ye, Ze 
+                     0., 0., 0.])                                   # Xe, Ye, Ze
 
 def get_trim(aircraft, airspeed, rho = RHO_SL_SLUG_FT3, gamma = 0.0):
     """
-    Obtain steady, level trim solution for a symmetric aircraft using pitch and thrust control. 
+    Obtain steady, level trim solution for a symmetric aircraft using pitch and thrust control.
 
     Parameters
     ----------
     aircraft : Aircraft
-        Aircraft object which implements "get_thrust," "get_mass_props," and 
+        Aircraft object which implements "get_thrust," "get_mass_props," and
         "get_aero_coeffs" properties
     airspeed : float
         True airspeed for trim
@@ -323,7 +323,7 @@ def get_trim(aircraft, airspeed, rho = RHO_SL_SLUG_FT3, gamma = 0.0):
     trim_pitch : float
         Trim pitch command in radians
     trim_throttle : foat
-        Trim throttle command, as a fraction of the return call to "get_thrust" 
+        Trim throttle command, as a fraction of the return call to "get_thrust"
     """
 
     # Due to the way controls are called, want to grab these placeholders to reset for lader
@@ -339,17 +339,17 @@ def get_trim(aircraft, airspeed, rho = RHO_SL_SLUG_FT3, gamma = 0.0):
         alpha, delta_pitch, thrust_frac = trim_params
 
         Xtrim = get_initial_state(airspeed, alpha, gamma = gamma)
-        
+
         aircraft.set_aero_controls(roll = lambda time, state: 0.,
                                    pitch = lambda time, state: delta_pitch,
                                    yaw = lambda time, state: 0.)
         aircraft.set_thrust(thrust_frac*Tx0, thrust_frac*Tz0)
-    
+
         states_dot = eom_6dof(0., Xtrim, aircraft, rho = RHO_SL_SLUG_FT3)
 
         return states_dot[[0, 2, 4]]
 
-    xout = fsolve(obj_fun, [0., 0., 1.])    
+    xout = fsolve(obj_fun, [0., 0., 1.])
 
     aircraft.set_aero_controls(roll = roll0, pitch = pitch0, yaw = yaw0)
     aircraft.set_thrust(Tx0, Tz0)
@@ -358,13 +358,13 @@ def get_trim(aircraft, airspeed, rho = RHO_SL_SLUG_FT3, gamma = 0.0):
 
 # Define an Aircraft for external use
 def GET_F106():
-    
+
     mass = 1000.
     weight = G_FT_S2*mass
     sref = 695
     bref = 38.13
     cref = 23.76
-    
+
     alpha0 = 11.5/180*np.pi   # Documented
     Cx0 = -0.02
     Cz0 = -0.05
@@ -373,10 +373,10 @@ def GET_F106():
     Cma = -0.16
     Cm0 = -0.05
     Cmdp = -0.30
-    
+
     Czdp = -0.56
     Cxdp = Cxa*(Czdp/Cza)  # Scaled estimate
-    
+
     # Trim aircraft at set alpha0
     # Fx = qinf*Sref*(Cx0 + Cxa*alpha + Cxdp*delta) + Tx - W*sin(theta0) = 0
     # Fz = qinf*Sref*(Cz0 + Cza*alpha + Czdp*delta) + W*cos(theta) = 0
@@ -389,29 +389,29 @@ def GET_F106():
     #
     CL0 = -0.316
     qinf = -weight/(sref*CL0)
-    
+
     theta0 = alpha0
-    A = np.array([[qinf*sref*Cxdp, 1.0, 0.0], 
+    A = np.array([[qinf*sref*Cxdp, 1.0, 0.0],
                   [qinf*sref*Czdp, 0.0, 0.0 ],
                   [qinf*sref*cref*Cmdp, 0.0, qinf*sref*cref]])
-    
+
     b = np.array([-qinf*sref*(Cx0 + Cxa*alpha0) + weight*np.sin(theta0),
                   -qinf*sref*(Cz0 + Cza*alpha0) - weight*np.cos(theta0),
                  -qinf*sref*cref*Cma*alpha0])
-    
+
     delta0, Tx0, Cm0 = np.linalg.solve(A, b)
 
     # sin_alpha = np.sin(alpha0)
     # cos_alpha = np.cos(alpha0)
-    
-    
+
+
     # CZ0 = Cz0 + Cza*alpha0 + Czdp*delta0
     # CX0 = Cxa*alpha0 + Cxdp*delta0
     # CL0 = -CZ0*cos_alpha + CX0*sin_alpha
     # CD0 = -CX0*cos_alpha - CZ0*sin_alpha
 
     # Lift = qinf*sref*CL ==> qinf = weight/(CL*sref)
-    
+
     # TODO: Get rid of this
     airspeed = np.sqrt(2*qinf/RHO_SL_SLUG_FT3)
 
@@ -419,7 +419,7 @@ def GET_F106():
     f106.set_mass_props(32170/G_FT_S2, 19000, 185000, 200000, Ixz = 6000)
     f106.set_ref_dims(sref, bref, cref)
     f106.set_thrust(Tx0)
-    
+
     f106.set_aero_coeffs(Cx0 = Cx0,
                          Cz0 = Cz0,
                          Cm0 = Cm0,
@@ -440,32 +440,32 @@ def GET_F106():
                          Cndr = -0.057,
                          Clp = -0.179,
                          Cnp = 0.0079,
-                         Clr = -0.084, 
+                         Clr = -0.084,
                          Cnr = -0.39)
-    
+
     return f106, airspeed
 
 if __name__ == '__main__':
-    
+
     import matplotlib.pyplot as plt
     from scipy.integrate import solve_ivp
 
     # Can we get the same trim solution using the function
     f106, veas = GET_F106()
     alpha0_rad, delta0_rad, throttle0 = get_trim(f106, veas)
-    
+
     print('Trim alpha [function] %.1f deg' % (alpha0_rad*57.3))
     print('Trim delta [function] %.1f deg' % (delta0_rad*57.3))
     print('Trim thrust fraction [function] %.3f' % (throttle0))
-    
+
     trim_X0 = get_initial_state(veas, alpha0_rad)
-    
-    
+
+
     # Output timesteps
     dt = 0.01
     T = 10.0
     time = np.arange(0, T, dt)
-    
+
     #################
     # Pitch Doublet #
     #################
@@ -473,46 +473,46 @@ if __name__ == '__main__':
     Tpitch = 2.0
     dpitch = 2.5/57.3
     pitch_history = lambda t, x: delta0_rad - dpitch*np.sin(np.pi*(t - pitch_start)/Tpitch)*(t > pitch_start)*(t < (pitch_start + Tpitch))
-    
+
     f106.set_aero_controls(pitch = pitch_history)
-    
+
     eom = lambda t, x: eom_6dof(t, x, f106)
     sol = solve_ivp(eom, [0, T], trim_X0, t_eval = time)
     u, v, w, p, q, r, phi, theta, psi, Xe, Ye, Ze = sol.y
     outputs = np.array([eom_6dof(ti, yi, f106, output_mode = True) for ti, yi in zip(time, sol.y.T)]).T
     V, alpha, beta, p, q, r, phi, theta, psi, xe, ye, ze, ax, ay, az, roll_control, pitch_control, yaw_control = outputs
-    
+
     lstyle = 'k-'
     fig, ax = plt.subplot_mosaic([['image', 'alpha [rad]'],
                                   ['image', 'q [rad/s]',],
                                   ['image', 'az [g]'],
                                   ['placehold', 'Pitch [rad]']],
-                                figsize = (10, 10), 
+                                figsize = (10, 10),
                                 constrained_layout = True)
-    
-    
+
+
     ax['alpha [rad]'].plot(sol.t, alpha, lstyle)
     ax['q [rad/s]'].plot(sol.t, q, lstyle)
     ax['az [g]'].plot(sol.t, az, lstyle)
     ax['Pitch [rad]'].plot(sol.t, pitch_control, lstyle)
-    
-    
+
+
     for key, a in ax.items():
         if key == 'image':
             continue
         a.set_xlabel('Time [s]', fontweight = 'bold')
         a.set_ylabel(key, fontweight = 'bold')
         a.grid()
-    
+
     # Read & plot image + add annotations
     image = plt.imread('images/f106_pitch_doublet.png')
     ax['image'].imshow(image)
     ax['image'].axis('off')
     ax['placehold'].axis('off')
-    
-    fig.suptitle('Simulated vs. As-Flown F106 Pitch Doublet Time Histories; M0.6 ~11 Deg AoA', fontweight = 'bold') 
+
+    fig.suptitle('Simulated vs. As-Flown F106 Pitch Doublet Time Histories; M0.6 ~11 Deg AoA', fontweight = 'bold')
     fig.savefig('plots/f106_pitch_doublet.png', bbox_inches = 'tight')
-    
+
     ###############
     # Rudder Kick #
     ###############
@@ -521,51 +521,51 @@ if __name__ == '__main__':
     drudder = 20/57.3
     yaw_history = lambda t, x: drudder*np.sin(np.pi*(t - rudder_start)/Trudder)*(t > rudder_start)*(t < (rudder_start + Trudder))
     pitch_history = lambda t, x: delta0_rad
-    
-    f106.set_aero_controls(pitch = pitch_history, 
+
+    f106.set_aero_controls(pitch = pitch_history,
                            yaw = yaw_history)
-    
+
     eom = lambda t, x: eom_6dof(t, x, f106)
-    
+
     sol = solve_ivp(eom, [0, T], trim_X0, t_eval = time)
     u, v, w, p, q, r, phi, theta, psi, Xe, Ye, Ze = sol.y
     outputs = np.array([eom_6dof(ti, yi, f106, output_mode = True) for ti, yi in zip(time, sol.y.T)]).T
     V, alpha, beta, p, q, r, phi, theta, psi, xe, ye, ze,  ax, ay, az, roll_control, pitch_control, yaw_control = outputs
-    
+
     lstyle = 'k-'
     fig, ax = plt.subplot_mosaic([['image', 'beta [rad]'],
                                   ['image', 'p [rad/s]',],
                                   ['image', 'r [rad/s]', ],
                                   ['image', 'ay [g]'],
                                   ['placehold', 'Rudder [rad]']],
-                                figsize = (10, 10), 
+                                figsize = (10, 10),
                                 constrained_layout = True)
-    
-    
+
+
     ax['beta [rad]'].plot(sol.t, beta, lstyle)
     ax['p [rad/s]'].plot(sol.t, p, lstyle)
     ax['r [rad/s]'].plot(sol.t, r, lstyle)
     ax['ay [g]'].plot(sol.t, ay, lstyle)
     ax['Rudder [rad]'].plot(sol.t, yaw_control, lstyle)
-    
-    
+
+
     for key, a in ax.items():
         if key == 'image':
             continue
         a.set_xlabel('Time [s]', fontweight = 'bold')
         a.set_ylabel(key, fontweight = 'bold')
         a.grid()
-    
+
     # Read & plot image + add annotations
     image = plt.imread('images/f106_yaw_doublet.png')
     ax['image'].imshow(image)
     ax['image'].axis('off')
     ax['placehold'].axis('off')
-    
-    fig.suptitle('Simulated vs. As-Flown F106 Rudder Kick Time Histories; M0.6 ~11 Deg AoA', fontweight = 'bold') 
+
+    fig.suptitle('Simulated vs. As-Flown F106 Rudder Kick Time Histories; M0.6 ~11 Deg AoA', fontweight = 'bold')
     fig.savefig('plots/f106_yaw_doublet.png', bbox_inches = 'tight')
-    
-    
-    
+
+
+
 
 
